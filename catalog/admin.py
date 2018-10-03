b741747c8075559757c1498000014adcf6e2e00c
Tom Booster <tom.booster@gmail.com>
2018-10-03 09:54:09 -0700

    Customize the Admin site. Close #7.

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Weight, BloodPressure
 
 
+class WeightAdmin(admin.ModelAdmin):
+    list_display = ('date', 'weight', 'body_fat', 'user')
+    list_filter = ('user', 'date')
+
+
 # Register your models here.
-admin.site.register(Weight)
+admin.site.register(Weight, WeightAdmin)
 admin.site.register(BloodPressure)
